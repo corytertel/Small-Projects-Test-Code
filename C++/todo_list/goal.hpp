28da0dddaee3aacba3dc77cec6447add6f162f33
corytertel <ctertel@comcast.net>
2021-10-07 21:00:54 -0700

    changes

--- C++/todo_list/goal.hpp
+++ C++/todo_list/goal.hpp
@@ -10,14 +10,14 @@ enum class GoalType {
 struct Goal {
     //Constructor
     Goal(GoalType type, std::string msg);
-    
+
     //Essentials: Destructor, Copy and Move Constructors, and Copy and Move Assignment Operators
     ~Goal();
     Goal(const Goal& other);
     Goal(Goal&& other) noexcept;
     Goal& operator=(const Goal& other);
     Goal& operator=(Goal&& other) noexcept;
-    
+
 private:
     GoalType type;
     std::string msg;
